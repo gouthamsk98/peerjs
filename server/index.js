90480c6e25625aedcc92ccec08eb987f62d4cc0c
goutham <goutham@biboxlabs.com>
2022-10-19 10:28:36 +0530

    basic connecion done

---
+++ server/index.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const { ExpressPeerServer } = require("peer");
+const cors = require("cors");
+
+const app = express();
+
+app.use(cors());
+
+app.get("/", (req, res, next) => res.send("Hello world!"));
+
+const server = app.listen(9000);
+
+const peerServer = ExpressPeerServer(server, {
+  debug: true,
+});
+
+app.use("/peerjs", peerServer);
+
+peerServer.on("connection", (client) => {
+  console.log("client connected");
+});
+
+peerServer.on("disconnect", (client) => {
+  console.log("client disconnected");
+});
